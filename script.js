c0fd70d9146852c1651f3ddfee28b7f4b2a8b2b2
jahleelnh06-dev <jahleelnh06@gmail.com>
2026-02-11 16:22:30 -0900

    Update script.js

--- script.js
+++ script.js
@@ -40,14 +40,14 @@ function flashRainbowColors(callback) {
     }, 2000); // Flash colors for 2 seconds
 }
 
-// Function to display the cat.gif initially
+// Function to display the me.png initially
 function displayCat() {
     // Get the container where the image will be displayed
     var imageContainer = document.getElementById('image-container');
     // Create a new Image element for the cat
     var catImage = new Image();
     // Set the source (file path) for the cat image
-    catImage.src = 'cat.gif'; // Assuming the cat image is named "cat.gif"
+    catImage.src = 'me.png'; // Assuming the cat image is named "me.png"
     // Set alternative text for the image (for accessibility)
     catImage.alt = 'Cat';
     // When the cat image is fully loaded, add it to the image container
@@ -76,5 +76,5 @@ function displayCatHeart() {
     };
 }
 
-// Display the cat.gif initially
+// Display the me.png initially
 displayCat();
